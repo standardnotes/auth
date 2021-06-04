cbb573e7d9ff29af2f5d43dafd7d0d63dabe1037
Karol Sójko <karolsojko@gmail.com>
2021-06-04 16:54:22 +0200

    feat: prevent MFA secret from leaking when retrieving user settings (#27)

--- src/Domain/UseCase/GetSetting/GetSetting.ts
+++ src/Domain/UseCase/GetSetting/GetSetting.ts
@@ -5,6 +5,7 @@ import { UseCaseInterface } from '../UseCaseInterface'
 import TYPES from '../../../Bootstrap/Types'
 import { SettingProjector } from '../../../Projection/SettingProjector'
 import { SettingRepositoryInterface } from '../../Setting/SettingRepositoryInterface'
+import { SETTINGS } from '../../Setting/Settings'
 
 @injectable()
 export class GetSetting implements UseCaseInterface {
@@ -15,8 +16,18 @@ export class GetSetting implements UseCaseInterface {
 
   async execute(dto: GetSettingDto): Promise<GetSettingResponse> {
     const { userUuid, settingName } = dto
+
+    if (settingName === SETTINGS.MFA_SECRET) {
+      return {
+        success: false,
+        error: {
+          message: `Setting ${settingName} for user ${userUuid} not found!`,
+        },
+      }
+    }
+
     const setting = await this.settingRepository.findOneByNameAndUserUuid(settingName, userUuid)
-    
+
     if (setting === undefined) {
       return {
         success: false,
@@ -30,7 +41,7 @@ export class GetSetting implements UseCaseInterface {
 
     return {
       success: true,
-      userUuid, 
+      userUuid,
       setting: simpleSetting,
     }
   }
--- src/Domain/UseCase/GetSettings/GetSettings.spec.ts
+++ src/Domain/UseCase/GetSettings/GetSettings.spec.ts
@@ -1,18 +1,21 @@
 import 'reflect-metadata'
+
 import { SettingProjectorTest } from '../../../Projection/test/SettingProjectorTest'
 import { Setting } from '../../Setting/Setting'
+import { SETTINGS } from '../../Setting/Settings'
 import { SimpleSetting } from '../../Setting/SimpleSetting'
 import { UserTest } from '../../User/test/UserTest'
 import { GetSettingsTest } from './test/GetSettingsTest'
 
 describe('GetSettings', () => {
-  const user = UserTest.makeWithSettings()
+  let user = UserTest.makeWithSettings()
   const userUuid = user.uuid
 
   const projector = SettingProjectorTest.get()
 
   let settings: Setting[]
   let simpleSettings: SimpleSetting[]
+
   beforeAll(async () => {
     settings = await user.settings
     simpleSettings = await projector.projectManySimple(settings)
@@ -24,6 +27,26 @@ describe('GetSettings', () => {
   })
 
   it('should get associated settings for a valid user uuid', async () => {
+    const actual = await makeSubject().execute({ userUuid })
+
+    expect(actual.userUuid).toEqual(userUuid)
+    expect(actual.settings).toEqual(simpleSettings)
+  })
+
+  it('should prevent mfa secret from being retrieved with all settings', async () => {
+    user = UserTest.makeSubject({
+      uuid: 'user-with-settings-uuid',
+    }, {
+      settings: [
+        { uuid: 'setting-1-uuid', name: 'setting-1-name' },
+        { uuid: 'setting-2-uuid', name: 'setting-2-name' },
+        { uuid: 'setting-3-uuid', name: SETTINGS.MFA_SECRET },
+      ],
+    })
+    settings = await user.settings
+    settings.pop()
+    simpleSettings = await projector.projectManySimple(settings)
+
     const actual = await makeSubject().execute({ userUuid })
 
     expect(actual.userUuid).toEqual(userUuid)
