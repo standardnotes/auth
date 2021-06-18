9d55877aecacab6e47eeb25bbd77a07b1d267fdf
Karol Sójko <karolsojko@gmail.com>
2021-06-18 15:58:06 +0200

    feat: removing disconnected web sockets connections (#31)

--- src/Controller/WebSocketsController.ts
+++ src/Controller/WebSocketsController.ts
@@ -3,6 +3,7 @@ import { inject } from 'inversify'
 import {
   BaseHttpController,
   controller,
+  httpDelete,
   httpPost,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   results,
@@ -10,11 +11,13 @@ import {
 import { Logger } from 'winston'
 import TYPES from '../Bootstrap/Types'
 import { AddWebSocketsConnection } from '../Domain/UseCase/AddWebSocketsConnection/AddWebSocketsConnection'
+import { RemoveWebSocketsConnection } from '../Domain/UseCase/RemoveWebSocketsConnection/RemoveWebSocketsConnection'
 
 @controller('/sockets')
 export class WebSocketsController extends BaseHttpController {
   constructor(
     @inject(TYPES.AddWebSocketsConnection) private addWebSocketsConnection: AddWebSocketsConnection,
+    @inject(TYPES.RemoveWebSocketsConnection) private removeWebSocketsConnection: RemoveWebSocketsConnection,
     @inject(TYPES.Logger) private logger: Logger
   ) {
     super()
@@ -32,6 +35,19 @@ export class WebSocketsController extends BaseHttpController {
       userUuid: request.body.userUuid,
       connectionId: request.body.connectionId,
     })
+
+    return this.json({ success: true })
+  }
+
+  @httpDelete('/')
+  async deleteWebSocketsConnection(request: Request): Promise<results.JsonResult | results.BadRequestErrorMessageResult> {
+    if (!request.body.connectionId) {
+      this.logger.debug('Missing required parameters from the request: %O', request.body)
+
+      return this.badRequest('Missing connection id')
+    }
+
+    await this.removeWebSocketsConnection.execute({ connectionId: request.body.connectionId })
 
     return this.json({ success: true })
   }
