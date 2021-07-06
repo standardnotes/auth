bb28252b5dfbf64e8dd39bcdf30fa3cf85f1f007
Karol Sójko <karolsojko@gmail.com>
2021-07-06 15:49:52 +0200

    feat: replace superagent with axios

--- src/Infra/HTTP/SyncingServerHttpService.spec.ts
+++ src/Infra/HTTP/SyncingServerHttpService.spec.ts
@@ -1,47 +1,44 @@
+import { AxiosInstance } from 'axios'
 import 'reflect-metadata'
 
-import { Response, SuperAgentRequest, SuperAgentStatic } from 'superagent'
 import { SyncingServerHttpService } from './SyncingServerHttpService'
 
 describe('SyncingServerHttpService', () => {
-  let httpClient: SuperAgentStatic
-  let request: SuperAgentRequest
-  let response: Response
+  let httpClient: AxiosInstance
   const authServerUrl = 'https://syncing-server'
 
   const createService = () => new SyncingServerHttpService(httpClient, authServerUrl)
 
   beforeEach(() => {
-    response = {} as jest.Mocked<Response>
-    response.status = 200
-    response.body = {
-      secret: 'foo',
-      extensionUuid: '1-2-3',
-    }
-
-    request = {} as jest.Mocked<SuperAgentRequest>
-    request.query = jest.fn().mockReturnThis()
-    request.ok = jest.fn().mockReturnThis()
-    request.send = jest.fn().mockReturnValue(response)
-
-    httpClient = {} as jest.Mocked<SuperAgentStatic>
-    httpClient.get = jest.fn().mockReturnValue(request)
+    httpClient = {} as jest.Mocked<AxiosInstance>
+    httpClient.request = jest.fn().mockReturnValue({ status: 200, data: { secret: 'foo', extensionUuid: '1-2-3' } })
   })
 
   it('should send a request to syncing service in order to get mfa secret for user', async () => {
     expect(await createService().getUserMFASecret('1-2-3')).toEqual({ secret: 'foo', extensionUuid: '1-2-3' })
 
-    expect(httpClient.get).toHaveBeenCalledWith('https://syncing-server/items/mfa/1-2-3')
-    expect(request.send).toHaveBeenCalled()
+    expect(httpClient.request).toHaveBeenCalledWith({
+      method: 'GET',
+      url: 'https://syncing-server/items/mfa/1-2-3',
+      headers: {
+        'Accept': 'application/json',
+      },
+      validateStatus: expect.any(Function),
+    })
   })
 
   it('should return undefined if mfa secret is not found for user', async () => {
-    response.status = 404
-    request.send = jest.fn().mockReturnValue(response)
+    httpClient.request = jest.fn().mockReturnValue({ status: 404 })
 
     expect(await createService().getUserMFASecret('1-2-3')).toEqual(undefined)
 
-    expect(httpClient.get).toHaveBeenCalledWith('https://syncing-server/items/mfa/1-2-3')
-    expect(request.send).toHaveBeenCalled()
+    expect(httpClient.request).toHaveBeenCalledWith({
+      method: 'GET',
+      url: 'https://syncing-server/items/mfa/1-2-3',
+      headers: {
+        'Accept': 'application/json',
+      },
+      validateStatus: expect.any(Function),
+    })
   })
 })
