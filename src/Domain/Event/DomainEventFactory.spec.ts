a37de91220630a4b0e3eadd16279b8752cb10382
Antonella Sgarlatta <antonella@standardnotes.org>
2021-07-28 12:39:27 -0300

    tests: add domain event factory test

--- src/Domain/Event/DomainEventFactory.spec.ts
+++ src/Domain/Event/DomainEventFactory.spec.ts
@@ -1,3 +1,4 @@
+import { RoleName } from '@standardnotes/auth'
 import 'reflect-metadata'
 
 import { DomainEventFactory } from './DomainEventFactory'
@@ -25,6 +26,21 @@ describe('DomainEventFactory', () => {
           userUuid: '1-2-3',
         },
         type: 'ACCOUNT_DELETION_REQUESTED',
+      })
+  })
+
+  it('should create a USER_ROLE_CHANGED event', () => {
+    expect(createFactory().createUserRoleChangedEvent('1-2-3', 'test@test.com', RoleName.CoreUser, RoleName.ProUser))
+      .toEqual({
+        createdAt: expect.any(Date),
+        payload: {
+          userUuid: '1-2-3',
+          email: 'test@test.com',
+          fromRole: RoleName.CoreUser,
+          toRole: RoleName.ProUser,
+          timestamp: expect.any(Number),
+        },
+        type: 'USER_ROLE_CHANGED',
       })
   })
 })
