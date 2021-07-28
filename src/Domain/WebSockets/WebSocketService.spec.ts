d7896f80f0ee22aa5f5e0bbe68c3f35cc6e9407d
Antonella Sgarlatta <antonella@standardnotes.org>
2021-07-28 12:14:16 -0300

    tests: add websockets service test

---
+++ src/Domain/WebSockets/WebSocketService.spec.ts
@@ -0,0 +1,68 @@
+import 'reflect-metadata'
+
+import { UserRoleChangedEvent } from '@standardnotes/domain-events'
+import { User } from '../User/User'
+import { RoleName } from '@standardnotes/auth'
+
+import { WebSocketsService } from './WebSocketsService'
+import { WebSocketsConnectionRepositoryInterface } from './WebSocketsConnectionRepositoryInterface'
+import { DomainEventFactoryInterface } from '../Event/DomainEventFactoryInterface'
+import { AxiosInstance } from 'axios'
+
+describe('WebSocketsService', () => {
+  let connectionIds: string[]
+  let user: User
+  let fromRole: RoleName
+  let toRole: RoleName
+  let event: UserRoleChangedEvent
+  let webSocketsConnectionRepository: WebSocketsConnectionRepositoryInterface
+  let domainEventFactory: DomainEventFactoryInterface
+  let httpClient: AxiosInstance
+  
+  const webSocketsApiUrl = 'http://test-websockets'
+
+  const createService = () => new WebSocketsService(
+    webSocketsConnectionRepository,
+    domainEventFactory,
+    httpClient,
+    webSocketsApiUrl,
+  )
+
+  beforeEach(() => {
+    connectionIds = ['1', '2']
+
+    user = {
+      uuid: '123',
+      email: 'test@test.com',
+    } as jest.Mocked<User>
+
+    fromRole = RoleName.CoreUser
+    toRole = RoleName.ProUser
+
+    event = {} as jest.Mocked<UserRoleChangedEvent>
+
+    webSocketsConnectionRepository = {} as jest.Mocked<WebSocketsConnectionRepositoryInterface>
+    webSocketsConnectionRepository.findAllByUserUuid = jest.fn().mockReturnValue(connectionIds)
+
+    domainEventFactory = {} as jest.Mocked<DomainEventFactoryInterface>
+    domainEventFactory.createUserRoleChangedEvent = jest.fn().mockReturnValue(event)
+
+    httpClient = {} as jest.Mocked<AxiosInstance>
+    httpClient.request = jest.fn()
+  })
+
+  describe('send user role changed event', () => {
+    it('should send a user role changed event to all user connections', async () => {
+      await createService().sendUserRoleChangedEvent(user, fromRole, toRole)
+
+      expect(httpClient.request).toHaveBeenCalledTimes(connectionIds.length)
+      connectionIds.map((id, index) => {
+        expect(httpClient.request).toHaveBeenNthCalledWith(index + 1, expect.objectContaining({
+          method: 'POST',
+          url: `${webSocketsApiUrl}/${id}`,
+          data: JSON.stringify(event),
+        }))
+      })
+    })
+  })
+})
