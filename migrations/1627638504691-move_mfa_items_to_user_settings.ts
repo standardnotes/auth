c9c7b743d687bec879faeb3600df0a62949bd280
Karol Sójko <karolsojko@gmail.com>
2021-07-30 12:12:50 +0200

    feat: add migration from MFA Items to MFA as a User Setting

---
+++ migrations/1627638504691-move_mfa_items_to_user_settings.ts
@@ -0,0 +1,69 @@
+import * as IORedis from 'ioredis'
+
+import { MfaSetting } from '@standardnotes/auth'
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+import { Setting } from '../src/Domain/Setting/Setting'
+import { User } from '../src/Domain/User/User'
+
+export class moveMfaItemsToUserSettings1627638504691 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const itemsTableExistsQueryResult = await queryRunner.manager.query('SELECT COUNT(*) as count FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = "items"')
+    const itemsTableExists = itemsTableExistsQueryResult[0].count === '1'
+    if (!itemsTableExists) {
+      return
+    }
+
+    const items = await queryRunner.manager.query('SELECT * FROM items WHERE content_type = "SF|MFA" ORDER BY updated_at_timestamp ASC')
+
+    const usersMFAStatus = new Map<string, number>()
+    const usersMFAUpdatedAt = new Map<string, number>()
+
+    for (const item of items) {
+      const user = await queryRunner.manager.findOne(User, item['user_uuid'])
+      if (user === undefined) {
+        continue
+      }
+
+      usersMFAStatus.set(item['user_uuid'], 1)
+      usersMFAUpdatedAt.set(item['user_uuid'], item['updated_at_timestamp'])
+
+      const setting = new Setting()
+      setting.uuid = item['uuid']
+      setting.name = MfaSetting.MfaSecret
+      setting.value = item['content']
+      if (item['deleted']) {
+        setting.value = null
+        usersMFAStatus.set(item['user_uuid'], 0)
+      }
+      setting.serverEncryptionVersion = Setting.ENCRYPTION_VERSION_UNENCRYPTED
+      setting.createdAt = item['created_at_timestamp']
+      setting.updatedAt = item['updated_at_timestamp']
+      setting.user = Promise.resolve(user)
+      await queryRunner.manager.save(setting)
+    }
+
+    const redisClient = this.getRedisClient()
+
+    for (const userUuid of usersMFAStatus.keys()) {
+      await redisClient.set(`mfa:${userUuid}`, usersMFAStatus.get(userUuid) as number)
+      await redisClient.set(`mfa_ua:${userUuid}`, usersMFAUpdatedAt.get(userUuid) as number)
+    }
+
+    await queryRunner.manager.query('DELETE FROM items WHERE content_type = "SF|MFA"')
+  }
+
+  public async down(): Promise<void> {
+    return
+  }
+
+  private getRedisClient(): IORedis.Redis | IORedis.Cluster {
+    const redisUrl = process.env.REDIS_URL as string
+    const isRedisInClusterMode = redisUrl.indexOf(',') > 0
+    if (isRedisInClusterMode) {
+      return new IORedis.Cluster(redisUrl.split(','))
+    }
+
+    return new IORedis(redisUrl)
+  }
+}
