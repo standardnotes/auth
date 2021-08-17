13973f14f0465875abd5b2d834635a942b469319
Karol Sójko <karolsojko@gmail.com>
2021-08-17 20:07:07 +0200

    feat: flatten encrypted mfa settings

---
+++ migrations/1629223072059-flatten_mfa_setting_and_encrypt.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+import { SnCryptoNode } from '@standardnotes/sncrypto-node'
+
+export class flattenMfaSettingAndEncrypt1629223072059 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const encryptedAndEncodedMFASettings = await queryRunner.manager.query('SELECT s.uuid as uuid, s.value as value, u.encrypted_server_key as encrypted_server_key FROM settings s LEFT JOIN users u ON u.uuid = s.user_uuid WHERE s.name = "MFA_SECRET" AND s.server_encryption_version = 2')
+
+    for (const encryptedAndEncodedMFASetting of encryptedAndEncodedMFASettings) {
+      const encodedMFASetting = await this.decryptMFASetting(encryptedAndEncodedMFASetting['value'], encryptedAndEncodedMFASetting['encrypted_server_key'])
+
+      const mfaSecret = this.getDecodedMFASecret(encodedMFASetting)
+
+      const encryptedMFASecret = await this.encryptMFASecret(mfaSecret, encryptedAndEncodedMFASetting['encrypted_server_key'])
+
+      await queryRunner.manager.query(`UDPATE settings SET value = "${encryptedMFASecret}", server_encryption_version = 1 WHERE uuid="${encryptedAndEncodedMFASetting['uuid']}"`)
+    }
+  }
+
+  public async down(): Promise<void> {
+    return
+  }
+
+  private async decryptMFASetting(encryptedMFASetting: string, userEncryptedServerKey: string) {
+    const crypto = new SnCryptoNode()
+
+    const userServerKey = JSON.parse(userEncryptedServerKey)
+
+    const decryptedUserServerKey = await crypto.aes256GcmDecrypt(userServerKey.encrypted, process.env.ENCRYPTION_SERVER_KEY as string)
+
+    const parsedVersionedEncrypted = JSON.parse(encryptedMFASetting)
+
+    return crypto.aes256GcmDecrypt(parsedVersionedEncrypted.encrypted, decryptedUserServerKey)
+  }
+
+  private async encryptMFASecret(secret: string, userEncryptedServerKey: string): Promise<string> {
+    const crypto = new SnCryptoNode()
+
+    const userServerKey = JSON.parse(userEncryptedServerKey)
+
+    const decryptedUserServerKey = await crypto.aes256GcmDecrypt(userServerKey.encrypted, process.env.ENCRYPTION_SERVER_KEY as string)
+
+    const iv = await crypto.generateRandomKey(128)
+
+    const encrypted = await crypto.aes256GcmEncrypt({
+      unencrypted: secret,
+      iv,
+      key: decryptedUserServerKey,
+    })
+
+    return JSON.stringify({ version: 1, encrypted })
+  }
+
+  private getDecodedMFASecret(encodedValue: string): string {
+    const valueBuffer = Buffer.from(encodedValue.substring(3), 'base64')
+    const decodedValue = valueBuffer.toString()
+
+    const decodedMFASecretObject = JSON.parse(decodedValue)
+
+    return decodedMFASecretObject.secret as string
+  }
+}
