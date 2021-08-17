16a570fe29fb96dc22042fce2eed2e2842651c22
Karol Sójko <karolsojko@gmail.com>
2021-08-17 19:49:07 +0200

    feat: encrypt encoded mfa settings

---
+++ migrations/1629217630132-encrypt_encoded_mfa_settings.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+import { SnCryptoNode } from '@standardnotes/sncrypto-node'
+
+export class encryptEncodedMfaSettings1629217630132 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const encodedMFASettings = await queryRunner.manager.query('SELECT s.uuid as uuid, s.value as value, u.encrypted_server_key as encrypted_server_key FROM settings s LEFT JOIN users u ON u.uuid = s.user_uuid WHERE s.name = "MFA_SECRET" AND s.server_encryption_version = 0')
+
+    for (const encodedMFASetting of encodedMFASettings) {
+      const mfaSecret = this.getDecodedMFASecret(encodedMFASetting['value'])
+
+      const encryptedMFASecret = await this.encryptMFASecret(mfaSecret, encodedMFASetting['encrypted_server_key'])
+
+      await queryRunner.manager.query(`UDPATE settings SET value = "${encryptedMFASecret}", server_encryption_version = 1 WHERE uuid="${encodedMFASetting['uuid']}"`)
+    }
+  }
+
+  public async down(): Promise<void> {
+    return
+  }
+
+  private async encryptMFASecret(secret: string, userEncryptedServerKey: string): Promise<string> {
+    const crypto = new SnCryptoNode()
+
+    const userServerKey = JSON.parse(userEncryptedServerKey)
+
+    const decryptedUserServerKey = await crypto.aes256GcmDecrypt(userServerKey.encrypted, process.env.ENCRYPTION_SERVER_KEY as string)
+
+    const iv = await crypto.generateRandomKey(128)
+
+    const encrypted = await crypto.aes256GcmEncrypt({
+      unencrypted: secret,
+      iv,
+      key: decryptedUserServerKey,
+    })
+
+    return JSON.stringify({ version: 1, encrypted })
+  }
+
+  private getDecodedMFASecret(encodedValue: string): string {
+    const valueBuffer = Buffer.from(encodedValue.substring(3), 'base64')
+    const decodedValue = valueBuffer.toString()
+
+    const decodedMFASecretObject = JSON.parse(decodedValue)
+
+    return decodedMFASecretObject.secret as string
+  }
+}
