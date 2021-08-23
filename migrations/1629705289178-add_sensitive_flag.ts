d54516a90e4583f7f92a680b547b81d8a2e571a2
Karol Sójko <karolsojko@gmail.com>
2021-08-23 11:15:22 +0200

    fix: migration query

--- migrations/1629705289178-add_sensitive_flag.ts
+++ migrations/1629705289178-add_sensitive_flag.ts
@@ -4,8 +4,8 @@ export class addSensitiveFlag1629705289178 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE `settings` ADD `sensitive` tinyint NOT NULL DEFAULT 0')
 
-    await queryRunner.query('UPDATE `settings` SET sensitive = 1 WHERE name = "MFA_SECRET"')
-    await queryRunner.query('UPDATE `settings` SET sensitive = 1 WHERE name = "EXTENSION_KEY"')
+    await queryRunner.query('UPDATE settings s SET s.sensitive = 1 WHERE s.name = "MFA_SECRET"')
+    await queryRunner.query('UPDATE settings s SET s.sensitive = 1 WHERE s.name = "EXTENSION_KEY"')
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
