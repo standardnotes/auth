061cfd63fc34dd015d0dfd58f6e0c93362c7294c
Karol Sójko <karolsojko@gmail.com>
2021-09-02 11:14:50 +0200

    refactor: extract features controller and settings controller from users controller (#54)
    * refactor: extract settings controller from users controller
    
    * refactor: extract features controller from users controller

--- src/Controller/UsersController.ts
+++ src/Controller/UsersController.ts
@@ -1,4 +1,3 @@
-import { SettingName } from '@standardnotes/settings'
 import { Request, Response } from 'express'
 import { inject } from 'inversify'
 import {
@@ -7,32 +6,20 @@ import {
   httpDelete,
   httpGet,
   httpPatch,
-  httpPut,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   results,
 } from 'inversify-express-utils'
 import TYPES from '../Bootstrap/Types'
-import { Setting } from '../Domain/Setting/Setting'
 import { DeleteAccount } from '../Domain/UseCase/DeleteAccount/DeleteAccount'
-import { DeleteSetting } from '../Domain/UseCase/DeleteSetting/DeleteSetting'
-import { GetSetting } from '../Domain/UseCase/GetSetting/GetSetting'
-import { GetSettings } from '../Domain/UseCase/GetSettings/GetSettings'
 import { GetUserKeyParams } from '../Domain/UseCase/GetUserKeyParams/GetUserKeyParams'
-import { UpdateSetting } from '../Domain/UseCase/UpdateSetting/UpdateSetting'
 import { UpdateUser } from '../Domain/UseCase/UpdateUser'
-import { GetUserFeatures } from '../Domain/UseCase/GetUserFeatures/GetUserFeatures'
 
 @controller('/users')
 export class UsersController extends BaseHttpController {
   constructor(
     @inject(TYPES.UpdateUser) private updateUser: UpdateUser,
-    @inject(TYPES.GetSettings) private doGetSettings: GetSettings,
-    @inject(TYPES.GetSetting) private doGetSetting: GetSetting,
-    @inject(TYPES.GetUserFeatures) private doGetUserFeatures: GetUserFeatures,
     @inject(TYPES.GetUserKeyParams) private getUserKeyParams: GetUserKeyParams,
-    @inject(TYPES.UpdateSetting) private doUpdateSetting: UpdateSetting,
     @inject(TYPES.DeleteAccount) private doDeleteAccount: DeleteAccount,
-    @inject(TYPES.DeleteSetting) private doDeleteSetting: DeleteSetting,
   ) {
     super()
   }
@@ -65,172 +52,6 @@ export class UsersController extends BaseHttpController {
     return this.json(updateResult.authResponse)
   }
 
-  @httpGet('/:userUuid/settings', TYPES.AuthMiddleware)
-  async getSettings(request: Request, response: Response): Promise<results.JsonResult> {
-    if (request.params.userUuid !== response.locals.user.uuid) {
-      return this.json({
-        error: {
-          message: 'Operation not allowed.',
-        },
-      }, 401)
-    }
-
-    const { userUuid } = request.params
-    const result = await this.doGetSettings.execute({ userUuid })
-
-    return this.json(result)
-  }
-
-  @httpGet('/:userUuid/mfa')
-  async getMFASettings(request: Request): Promise<results.JsonResult> {
-    const result = await this.doGetSettings.execute({
-      userUuid: request.params.userUuid,
-      settingName: SettingName.MfaSecret,
-      allowSensitiveRetrieval: true,
-      updatedAfter: request.body.lastSyncTime,
-    })
-
-    if (result.success) {
-      return this.json(result)
-    }
-
-    return this.json(result, 400)
-  }
-
-  @httpDelete('/:userUuid/mfa',)
-  async deleteMFASetting(request: Request): Promise<results.JsonResult> {
-    const { userUuid } = request.params
-    const { uuid, updatedAt } = request.body
-
-    const result = await this.doDeleteSetting.execute({
-      uuid,
-      userUuid,
-      settingName: SettingName.MfaSecret,
-      timestamp: updatedAt,
-      softDelete: true,
-    })
-
-    if (result.success) {
-      return this.json(result)
-    }
-
-    return this.json(result, 400)
-  }
-
-  @httpPut('/:userUuid/mfa')
-  async updateMFASetting(request: Request): Promise<results.JsonResult | results.StatusCodeResult> {
-    const {
-      uuid,
-      value,
-      serverEncryptionVersion = Setting.ENCRYPTION_VERSION_CLIENT_ENCODED_AND_SERVER_ENCRYPTED,
-      createdAt,
-      updatedAt,
-    } = request.body
-
-    const props = {
-      uuid,
-      value,
-      serverEncryptionVersion,
-      name: SettingName.MfaSecret,
-      createdAt,
-      updatedAt,
-      sensitive: true,
-    }
-
-    const { userUuid } = request.params
-    const result = await this.doUpdateSetting.execute({
-      userUuid,
-      props,
-    })
-
-    if (result.success) {
-      return this.json({ setting: result.setting }, result.statusCode)
-    }
-
-    return this.json(result, 400)
-  }
-
-  @httpGet('/:userUuid/settings/:settingName', TYPES.AuthMiddleware)
-  async getSetting(request: Request, response: Response): Promise<results.JsonResult> {
-    if (request.params.userUuid !== response.locals.user.uuid) {
-      return this.json({
-        error: {
-          message: 'Operation not allowed.',
-        },
-      }, 401)
-    }
-
-    const { userUuid, settingName } = request.params
-    const result = await this.doGetSetting.execute({ userUuid, settingName })
-
-    if (result.success) {
-      return this.json(result)
-    }
-
-    return this.json(result, 400)
-  }
-
-  @httpPut('/:userUuid/settings', TYPES.AuthMiddleware)
-  async updateSetting(request: Request, response: Response): Promise<results.JsonResult | results.StatusCodeResult> {
-    if (request.params.userUuid !== response.locals.user.uuid) {
-      return this.json({
-        error: {
-          message: 'Operation not allowed.',
-        },
-      }, 401)
-    }
-
-    const {
-      name,
-      value,
-      serverEncryptionVersion = Setting.ENCRYPTION_VERSION_DEFAULT,
-      sensitive = false,
-    } = request.body
-
-    const props = {
-      name,
-      value,
-      serverEncryptionVersion,
-      sensitive,
-    }
-
-    const { userUuid } = request.params
-    const result = await this.doUpdateSetting.execute({
-      userUuid,
-      props,
-    })
-
-    if (result.success) {
-      return this.json({ setting: result.setting }, result.statusCode)
-    }
-
-    return this.json(result, 400)
-  }
-
-  @httpDelete('/:userUuid/settings/:settingName', TYPES.AuthMiddleware)
-  async deleteSetting(request: Request, response: Response): Promise<results.JsonResult> {
-    if (request.params.userUuid !== response.locals.user.uuid) {
-      return this.json({
-        error: {
-          message: 'Operation not allowed.',
-        },
-      }, 401)
-    }
-
-    const { userUuid, settingName } = request.params
-
-    const result = await this.doDeleteSetting.execute({
-      userUuid,
-      settingName,
-    })
-
-    if (result.success) {
-      return this.json(result)
-    }
-
-    return this.json(result, 400)
-  }
-
   @httpGet('/params')
   async keyParams(request: Request): Promise<results.JsonResult> {
     const email = 'email' in request.query ? <string> request.query.email : undefined
@@ -260,26 +81,5 @@ export class UsersController extends BaseHttpController {
     })
 
     return this.json({ message: result.message }, result.responseCode)
-  }
-
-  @httpGet('/:userUuid/features', TYPES.AuthMiddleware)
-  async getFeatures(request: Request, response: Response): Promise<results.JsonResult> {
-    if (request.params.userUuid !== response.locals.user.uuid) {
-      return this.json({
-        error: {
-          message: 'Operation not allowed.',
-        },
-      }, 401)
-    }
-
-    const result = await this.doGetUserFeatures.execute({
-      userUuid: request.params.userUuid,
-    })
-
-    if (result.success) {
-      return this.json(result)
-    }
-
-    return this.json(result, 400)
   }
 }
