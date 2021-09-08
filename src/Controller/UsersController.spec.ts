f10cf9ba3eabb79c9d1ea5c6ba8de8d7792e2952
Karol Sójko <karolsojko@gmail.com>
2021-09-08 09:34:39 +0200

    fix: move change credentials endpoint to be consistent with api gateway

--- src/Controller/UsersController.spec.ts
+++ src/Controller/UsersController.spec.ts
@@ -9,12 +9,18 @@ import { UpdateUser } from '../Domain/UseCase/UpdateUser'
 import { GetUserKeyParams } from '../Domain/UseCase/GetUserKeyParams/GetUserKeyParams'
 import { DeleteAccount } from '../Domain/UseCase/DeleteAccount/DeleteAccount'
 import { GetUserSubscription } from '../Domain/UseCase/GetUserSubscription/GetUserSubscription'
+import { ClearLoginAttempts } from '../Domain/UseCase/ClearLoginAttempts'
+import { IncreaseLoginAttempts } from '../Domain/UseCase/IncreaseLoginAttempts'
+import { ChangeCredentials } from '../Domain/UseCase/ChangeCredentials/ChangeCredentials'
 
 describe('UsersController', () => {
   let updateUser: UpdateUser
   let deleteAccount: DeleteAccount
   let getUserKeyParams: GetUserKeyParams
   let getUserSubscription: GetUserSubscription
+  let clearLoginAttempts: ClearLoginAttempts
+  let increaseLoginAttempts: IncreaseLoginAttempts
+  let changeCredentials: ChangeCredentials
 
   let request: express.Request
   let response: express.Response
@@ -25,6 +31,9 @@ describe('UsersController', () => {
     getUserKeyParams,
     deleteAccount,
     getUserSubscription,
+    clearLoginAttempts,
+    increaseLoginAttempts,
+    changeCredentials
   )
 
   beforeEach(() => {
@@ -36,12 +45,22 @@ describe('UsersController', () => {
 
     user = {} as jest.Mocked<User>
     user.uuid = '123'
+    user.email = 'test@test.te'
 
     getUserKeyParams = {} as jest.Mocked<GetUserKeyParams>
     getUserKeyParams.execute = jest.fn()
 
     getUserSubscription = {} as jest.Mocked<GetUserSubscription>
     getUserSubscription.execute = jest.fn()
+
+    changeCredentials = {} as jest.Mocked<ChangeCredentials>
+    changeCredentials.execute = jest.fn()
+
+    clearLoginAttempts = {} as jest.Mocked<ClearLoginAttempts>
+    clearLoginAttempts.execute = jest.fn()
+
+    increaseLoginAttempts = {} as jest.Mocked<IncreaseLoginAttempts>
+    increaseLoginAttempts.execute = jest.fn()
 
     request = {
       headers: {},
@@ -74,6 +93,7 @@ describe('UsersController', () => {
       version: '002',
       user: {
         uuid: '123',
+        email: 'test@test.te',
       },
     })
 
@@ -101,6 +121,7 @@ describe('UsersController', () => {
       version: '002',
       user: {
         uuid: '123',
+        email: 'test@test.te',
       },
     })
 
@@ -245,5 +266,113 @@ describe('UsersController', () => {
     expect(getUserSubscription.execute).not.toHaveBeenCalled()
 
     expect(result.statusCode).toEqual(401)
+  })
+
+  it('should change a password', async () => {
+    request.body.version = '004'
+    request.body.api = '20190520'
+    request.body.current_password = 'test123'
+    request.body.new_password = 'test234'
+    request.body.pw_nonce = 'asdzxc'
+    request.body.origination = 'change-password'
+    request.body.created = '123'
+    request.headers['user-agent'] = 'Google Chrome'
+    response.locals.user = user
+
+    changeCredentials.execute = jest.fn().mockReturnValue({ success: true, authResponse: { foo: 'bar' } })
+
+    const httpResponse = <results.JsonResult> await createController().changeCredentials(request, response)
+    const result = await httpResponse.executeAsync()
+
+    expect(changeCredentials.execute).toHaveBeenCalledWith({
+      apiVersion: '20190520',
+      updatedWithUserAgent: 'Google Chrome',
+      currentPassword: 'test123',
+      newPassword: 'test234',
+      kpCreated: '123',
+      kpOrigination: 'change-password',
+      pwNonce: 'asdzxc',
+      protocolVersion: '004',
+      user: {
+        uuid: '123',
+        email: 'test@test.te',
+      },
+    })
+
+    expect(clearLoginAttempts.execute).toHaveBeenCalled()
+
+    expect(result.statusCode).toEqual(200)
+    expect(await result.content.readAsStringAsync()).toEqual('{"foo":"bar"}')
+  })
+
+  it('should indicate if changing a password fails', async () => {
+    request.body.version = '004'
+    request.body.api = '20190520'
+    request.body.current_password = 'test123'
+    request.body.new_password = 'test234'
+    request.body.pw_nonce = 'asdzxc'
+    request.headers['user-agent'] = 'Google Chrome'
+    response.locals.user = user
+
+    changeCredentials.execute = jest.fn().mockReturnValue({ success: false, errorMessage: 'Something bad happened' })
+
+    const httpResponse = <results.JsonResult> await createController().changeCredentials(request, response)
+    const result = await httpResponse.executeAsync()
+
+    expect(increaseLoginAttempts.execute).toHaveBeenCalled()
+
+    expect(result.statusCode).toEqual(401)
+    expect(await result.content.readAsStringAsync()).toEqual('{"error":{"message":"Something bad happened"}}')
+  })
+
+  it('should not change a password if current password is missing', async () => {
+    request.body.version = '004'
+    request.body.api = '20190520'
+    request.body.new_password = 'test234'
+    request.body.pw_nonce = 'asdzxc'
+    request.headers['user-agent'] = 'Google Chrome'
+    response.locals.user = user
+
+    const httpResponse = <results.JsonResult> await createController().changeCredentials(request, response)
+    const result = await httpResponse.executeAsync()
+
+    expect(changeCredentials.execute).not.toHaveBeenCalled()
+
+    expect(result.statusCode).toEqual(400)
+    expect(await result.content.readAsStringAsync()).toEqual('{"error":{"message":"Your current password is required to change your password. Please update your application if you do not see this option."}}')
+  })
+
+  it('should not change a password if new password is missing', async () => {
+    request.body.version = '004'
+    request.body.api = '20190520'
+    request.body.current_password = 'test123'
+    request.body.pw_nonce = 'asdzxc'
+    request.headers['user-agent'] = 'Google Chrome'
+    response.locals.user = user
+
+    const httpResponse = <results.JsonResult> await createController().changeCredentials(request, response)
+    const result = await httpResponse.executeAsync()
+
+    expect(changeCredentials.execute).not.toHaveBeenCalled()
+
+    expect(result.statusCode).toEqual(400)
+    expect(await result.content.readAsStringAsync()).toEqual('{"error":{"message":"Your new password is required to change your password. Please try again."}}')
+  })
+
+  it('should not change a password if password nonce is missing', async () => {
+    request.body.version = '004'
+    request.body.api = '20190520'
+    request.body.current_password = 'test123'
+    request.body.new_password = 'test234'
+    request.headers['user-agent'] = 'Google Chrome'
+    response.locals.user = user
+
+    const httpResponse = <results.JsonResult> await createController().changeCredentials(request, response)
+    const result = await httpResponse.executeAsync()
+
+    expect(changeCredentials.execute).not.toHaveBeenCalled()
+
+    expect(result.statusCode).toEqual(400)
+    expect(await result.content.readAsStringAsync()).toEqual('{"error":{"message":"The change password request is missing new auth parameters. Please try again."}}')
   })
 })
