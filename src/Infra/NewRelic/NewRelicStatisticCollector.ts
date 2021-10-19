7fb295b3c1812be0e8aafb48bc62935fa817752b
Karol Sójko <karolsojko@gmail.com>
2021-10-19 16:36:21 +0200

    wip

--- src/Infra/NewRelic/NewRelicStatisticCollector.ts
+++ src/Infra/NewRelic/NewRelicStatisticCollector.ts
@@ -1,3 +1,4 @@
+import { AxiosInstance } from 'axios'
 import { inject, injectable } from 'inversify'
 import TYPES from '../../Bootstrap/Types'
 import { StatisticCollectorInterface } from '../../Domain/Statistic/StatisticCollectorInterface'
@@ -6,11 +7,17 @@ import { StatisticName } from '../../Domain/Statistic/StatisticName'
 @injectable()
 export class NewRelicStatisticCollector implements StatisticCollectorInterface {
   constructor (
-    @inject(TYPES.StatisticRecordingFunction) private newrelicStatisticRecordingFunction: (eventType: string, attributes: { [keys: string]: boolean | number | string }) => void
+    @inject(TYPES.HTTPClient) private httpClient: AxiosInstance,
   ) {
   }
 
-  recordStatistic(statisticName: StatisticName, attributes: { [keys: string]: string | number | boolean }): void {
-    this.newrelicStatisticRecordingFunction(statisticName, attributes)
+  async recordStatistic(statisticName: StatisticName, attributes: { [keys: string]: string | number | boolean }): void {
+    await this.httpClient.request({
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
   }
 }
